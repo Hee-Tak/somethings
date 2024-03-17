6925c51779f5d8251eaf916f80570149c37e2163
hitak <hitak77@naver.com>
2024-03-17 19:41:14 +0900

    네이버 로그인 시도 중

--- app/src/main/java/com/tak/something/NaverLogin.kt
+++ app/src/main/java/com/tak/something/NaverLogin.kt
@@ -2,10 +2,21 @@ package com.tak.something
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tak.something.databinding.ActivityNaverLoginBinding
 
 class NaverLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNaverLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_naver_login)
+
+        binding = ActivityNaverLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /** Naver Login Module Initialize */
+        val naverClientId = getString(R.string.social_login_info_naver_client_id)
+        val naverClientSecret = getString(R.string.social_login_info_naver_client_secret)
+        val naverClientName = getString(R.string.social_login_info_naver_client_name)
+
     }
 }
