cc09e58fe089a8607c63c79d91d1ccb6d780fc40
hitak <hitak77@naver.com>
2024-03-18 19:19:27 +0900

    네이버 로그인 시도 중

--- app/src/main/java/com/tak/something/NaverLogin.kt
+++ app/src/main/java/com/tak/something/NaverLogin.kt
@@ -2,6 +2,8 @@ package com.tak.something
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.navercorp.nid.NaverIdLoginSDK
 import com.tak.something.databinding.ActivityNaverLoginBinding
 
 class NaverLogin : AppCompatActivity() {
@@ -17,6 +19,23 @@ class NaverLogin : AppCompatActivity() {
         val naverClientId = getString(R.string.social_login_info_naver_client_id)
         val naverClientSecret = getString(R.string.social_login_info_naver_client_secret)
         val naverClientName = getString(R.string.social_login_info_naver_client_name)
+        NaverIdLoginSDK.initialize(this, naverClientId, naverClientSecret, naverClientName)
 
+
+
+    }
+
+
+    private fun setLayoutState(login: Boolean){
+        if(login){
+            binding.tvNaverLogin.visibility = View.GONE
+            binding.tvNaverLogout.visibility = View.VISIBLE
+            binding.tvNaverDeleteToken.visibility = View.VISIBLE
+        } else {
+            binding.tvNaverLogin.visibility = View.VISIBLE
+            binding.tvNaverLogout.visibility = View.GONE
+            binding.tvNaverDeleteToken.visibility = View.GONE
+            binding.tvResult.text = ""
+        }
     }
 }
