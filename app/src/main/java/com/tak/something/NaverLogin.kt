9579d6f8a4127e8f2a176d96a642fe2ac7b48062
hitak <hitak77@naver.com>
2024-03-19 00:55:14 +0900

    네이버 로그인 시도 중

--- app/src/main/java/com/tak/something/NaverLogin.kt
+++ app/src/main/java/com/tak/something/NaverLogin.kt
@@ -2,8 +2,12 @@ package com.tak.something
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
 import com.tak.something.databinding.ActivityNaverLoginBinding
 
 class NaverLogin : AppCompatActivity() {
@@ -23,6 +27,36 @@ class NaverLogin : AppCompatActivity() {
 
 
 
+    }
+
+
+    /**
+     * 연동해제
+     * 네이버 아이디와 애플리케이션의 연동을 해제하는 기능은 다음과 같이
+     * NidOAuthLogin().callDeleteTokenApi() 메서드로 구현합니다.
+     * 연동을 해제하면 클라이언트에 저장된 토큰과 서버에 저장된 토큰이 모두 삭제됩니다.
+     */
+    private fun startNaverDeleteToken(){
+        NidOAuthLogin().callDeleteTokenApi(this, object : OAuthLoginCallback{
+            override fun onSuccess() {
+                //서버에서 토큰 삭제에 성공한 상태입니다.
+                setLayoutState(false)
+                Toast.makeText(this@NaverLogin, "네이버 아이디 토큰삭제 성공!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+                //서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                //클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                Log.d("naver", "errorCode: ${NaverIdLoginSDK.getLastErrorCode().code}")
+                Log.d("naver", "errorDesc: ${NaverIdLoginSDK.getLastErrorDescription()}")
+            }
+
+            override fun onError(errorCode: Int, message: String) {
+                //서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                //클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                onFailure(errorCode, message)
+            }
+        })
     }
 
 
