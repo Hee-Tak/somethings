bbac0f5e9fcbb3a8d0eb1b70717c0949009c2dcc
hitak <hitak77@naver.com>
2024-03-20 01:42:08 +0900

    네이버 로그인 시도 중

--- app/src/main/java/com/tak/something/NaverLogin.kt
+++ app/src/main/java/com/tak/something/NaverLogin.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 import com.navercorp.nid.NaverIdLoginSDK
 import com.navercorp.nid.oauth.NidOAuthLogin
 import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
 import com.tak.something.databinding.ActivityNaverLoginBinding
 
 class NaverLogin : AppCompatActivity() {
@@ -25,8 +27,91 @@ class NaverLogin : AppCompatActivity() {
         val naverClientName = getString(R.string.social_login_info_naver_client_name)
         NaverIdLoginSDK.initialize(this, naverClientId, naverClientSecret, naverClientName)
 
+        setLayoutState(false)
+
+        binding.tvNaverLogin.setOnClickListener {
+            startNaverLogin()
+        }
+        binding.tvNaverLogout.setOnClickListener {
+            startNaverLogout()
+        }
+        binding.tvNaverDeleteToken.setOnClickListener {
+            startNaverDeleteToken()
+        }
 
 
+    }
+
+
+
+    /**
+     * 로그인
+     * authenticate() 메서드를 히용한 로그인
+     */
+    private fun startNaverLogin() {
+        var naverToken : String? = ""
+
+        val profileCallback = object : NidProfileCallback<NidProfileResponse> {
+            override fun onSuccess(response: NidProfileResponse) {
+                val userId = response.profile?.id
+                binding.tvResult.text = "id: ${userId} \ntoken: ${naverToken}"
+                setLayoutState(true)
+                Toast.makeText(this@NaverLogin, "네이버 아이디 로그인 성공!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Toast.makeText(this@NaverLogin, "errorCode : ${errorCode}\n" + "errorDescription : ${errorDescription}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+        }
+
+        /**
+         * OAuthLoginCallback을 authenticate() 메서드 호출 시 파라미터로 전달하거나 NidOAuthLoginButton 객체에 등록하면 인증이 종료되는 것을 확인할 수 있습니다.
+         */
+        val oauthLoginCallback = object : OAuthLoginCallback {
+            override fun onSuccess() {
+                //네이버 로그인 인증이 성공했을 때 수행할 코드 추가
+                naverToken = NaverIdLoginSDK.getAccessToken()
+                //var naverRefreshToken = NaverIdLoginSDK.getRefreshToken()
+                //var naverExpiresAt = NaverIdLoginSDK.getExpiresAt().toString()
+                //var naverTokenType = NaverIdLoginSDK.getTokenType()
+                //var naverState = NaverIdLoginSDK.getState().toString()
+
+                //로그인 유저 정보 가져오기
+                NidOAuthLogin().callProfileApi(profileCallback)
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Toast.makeText(this@NaverLogin, "errorCode : ${errorCode}\n" + "errorDescription : ${errorDescription}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+        }
+
+        NaverIdLoginSDK.authenticate(this, oauthLoginCallback)
+    }
+
+
+
+
+
+    /**
+     * 로그아웃
+     * 애플리케이션에서 로그아웃할 때는 다음과 같이 NaverIdLoginSDK.logout() 메서드를 호출합니다.
+     */
+    private fun startNaverLogout() {
+        NaverIdLoginSDK.logout()
+        setLayoutState(false)
+        Toast.makeText(this@NaverLogin, "네이버 아이디 로그아웃 성공!", Toast.LENGTH_SHORT).show()
     }
 
 
